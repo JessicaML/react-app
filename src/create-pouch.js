4c66f8674a6e5f31f25369a2260f73ffa7283c0c
Jessica Leach <jessicamaryleach@gmail.com>
2017-08-01 16:21:32 +0200

    added psuedo code in create-pouch.js

---
+++ src/create-pouch.js
@@ -0,0 +1,14 @@
+//fetch data from mongo
+
+
+//set timeout (every 60 mins?)
+
+
+// compare mongo data with pouch data
+
+
+//if there is a new mongo db doc, create a new corresponding pouch doc
+
+
+
+
