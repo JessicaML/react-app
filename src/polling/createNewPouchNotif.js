7470e79451b6a6cf92a0030da70a78beec42464c
Jessica Leach <jessicamaryleach@gmail.com>
2017-08-09 10:46:03 +0200

    return difference from 2 arrays, insert difference to pouch

--- src/polling/createNewPouchNotif.js
+++ src/polling/createNewPouchNotif.js
@@ -1,6 +1,8 @@
 import returnDiffArrays from './returnDiffArrays'
 import fetchNewNotifs from './fetchNewNotifs'
 import db from "../index-pouch.js";
+var PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-upsert'));
 
 
 // get pouch data
@@ -15,49 +17,20 @@ export default function createNewPouchNotif(itemToCount) {
             startkey: "notif",
             endkey: "notif\ufff0"
         })
-        // get JSON dataz
         .then(res => {
-            console.log("pouch notifs:", res);
-            // console.log(res.rows[0])
             const jsonData = fetchNewNotifs()
                 .then(jsonData => {
-                    console.log("bah res", res.rows)
-                    let pouchNotifs = res.rows
-                    console.log("bah jsonData", jsonData.jobs)
-                    let mongoNotifs = jsonData.jobs
-                    console.log("a", mongoNotifs)
-                    console.log("b", pouchNotifs)
-
-                    const diff = returnDiffArrays(pouchNotifs, mongoNotifs).then(console.log);
-
-                    // res["rows2"] = jsonData;
+                    return returnDiffArrays(jsonData.jobs, res.rows) 
                 }).then(res => {
                     console.log("jsondata", res)
-                    // }).then(res => {
-
-
-                    // push jsonData onto 
-                    // const diff = returnDiffArrays([1,2,3,4], [1,2,3]).then(console.log);
+                    db.put({
+                        "_id": "notif_" + "26",
+                        "title": res[0].title,
+                        "body": res[0].description,
+                        "viewed": false,
+                    })
                 }).catch(err => console.log(err));
-            // console.log("jsonData", jsonData)
-            // returnDiffArrays(res, jsonData); 
-            // console.log("returnDiffArrays");
-            // console.log('fetchNewNotifs', );
         })
-        // compare 2 arrays
-
-        //insert to pouch
-        //    .then(notif => {
-        //         db.upsert({
-        //             "_id": "notif_" + "22",
-        //             "title": notif.title,
-        //             "body": notif.description,
-        //             "viewed": false,
-        //         })      
-        // })
-        .catch(err => console.log(err));
-
     // compare 2 arrays
-
 
 }
