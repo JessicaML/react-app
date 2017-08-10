519010fe8819b770fb293e90d677a889b472c8e5
Jessica Leach <jessicamaryleach@gmail.com>
2017-08-10 09:07:12 +0200

    compare arrays function returns only new  docs found in json data

--- src/polling/returnDiffArrays.js
+++ src/polling/returnDiffArrays.js
@@ -1,27 +1,18 @@
 export default function returnDiffArrays(a1, a2) {
-// let pouchDoc = a2.doc 
     function url(otherArray) {
+        console.log('otherArray', otherArray)
         return function(current) {
-            console.log("current.doc", current.doc)
-            let pouchDoc = current.doc;
-            return otherArray.filter(function(other, pouchDoc) {
-                // console.log("other.title", other.title)
-                console.log("pouchDoc", current.doc)
-                
-                return other.title == current.title
+        console.log('current', current)
+            return otherArray.filter(function(other) {
+                console.log('other', other)
+                let pouchDoc = other.doc;
+                console.log("pouchDoc.title", pouchDoc.title)        
+                return pouchDoc.title == current.title
             }).length == 0;
         }
     }
 
-    var onlyInA = a1.filter(url(a2));
-    var onlyInB = a2.filter(url(a1));
-
-    var result = onlyInA.concat(onlyInB);
-
-    // console.log("result is", result);
-    // console.log("res title", result[0].title);
+    var result = a1.filter(url(a2));
     return result;
 
 }
-
-
